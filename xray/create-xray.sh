171be2d5cdf8f8749dc981ce92261858b1486ef3
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2024-05-11 23:14:46 +0700

    Update create-xray.sh

--- xray/create-xray.sh
+++ xray/create-xray.sh
@@ -1,3 +1,12 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
 user=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1`
 domain=$(cat /usr/local/etc/xray/domain)
 cipher="aes-256-gcm"
