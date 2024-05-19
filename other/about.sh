0bf89a417d753edaed35dd5db6dcddcb946abd9e
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2024-05-19 21:02:35 +0700

    Update about.sh

--- other/about.sh
+++ other/about.sh
@@ -11,8 +11,6 @@ clear
 echo ""
 echo ""
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
-echo -e "                  ${WB}XRAY SCRIPT BY DUGONG${NC}"
-echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
 echo -e "                 ${WB}»»» Protocol Service «««${NC}  "
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
 echo -e "${YB}Vmess Websocket${NC}     : ${YB}443 & 80${NC}"
