e6957ebf3feb08c74912191317e848fc798fd3d7
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2024-06-05 10:18:27 +0700

    Create xp.sh

---
+++ testing/other/xp.sh
@@ -0,0 +1,17 @@
+clear
+# ALLXRAY
+data=($(cat /usr/local/etc/xray/config.json | grep '^#&@' | cut -d ' ' -f 2 | sort | uniq))
+now=$(date +"%Y-%m-%d")
+for user in "${data[@]}"; do
+exp=$(grep -w "^#&@ $user" "/usr/local/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(((d1 - d2) / 86400))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^#&@ $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+sed -i "/^#&@ $user $exp/,/^},{/d" /usr/local/etc/xray/config.json
+rm -rf /var/www/html/xray/xray-$user.log
+rm -rf /user/xray-$user.log
+systemctl restart xray
+fi
+done
