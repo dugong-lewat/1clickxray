b44bd8363eeeb2df384ef65d2c0632490c1a9580
Dugong <110967975+dugong-lewat@users.noreply.github.com>
2024-09-05 17:45:49 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ sleep 1
 print_msg $YB "Memasang build-essential dan dependensi lainnya..."
 apt install build-essential libpcre3 libpcre3-dev zlib1g zlib1g-dev openssl libssl-dev gcc clang llvm g++ valgrind make cmake debian-keyring debian-archive-keyring apt-transport-https systemd bind9-host gnupg2 ca-certificates lsb-release ubuntu-keyring debian-archive-keyring -y
 apt install unzip python-is-python3 python3-pip -y
-pip install psutil pandas tabulate rich py-cpuinfo distro requests pycountry geoip2 #--break-system-packages
+pip install psutil pandas tabulate rich py-cpuinfo distro requests pycountry geoip2 --break-system-packages
 check_success
 sleep 1
 
